dd6037d54707aad0e164357e6ef0a30def7cf805
vitocatz <40641469+vitorcato@users.noreply.github.com>
2023-06-18 22:58:06 -0300

    Create pup.js for my-project WPP

---
+++ pup.js
@@ -0,0 +1,13 @@
+async function resumir_cidade(texto, comprimento_resumo = 50) {
+    const prompt = `Redija um texto sobre a cidade: ${cidade} contanto sua historia, população, riqueza e fatos importantes. No máximo de 4 parágrafos, com coerência, ortografia perfeita e ótimo para leitura.\nComprimento: ${comprimento_resumo}\n`;
+  const completions = await openai.completions.create({
+    engine: 'text-davinci-002',
+    prompt,
+    max_tokens: comprimento_resumo,
+    temperature: 0.5,
+  });
+  const resumo = completions.choices[0].text.trim();
+  return resumo;
+}
+
+resumir_cidade("Camaçari")
