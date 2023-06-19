6cb5c2d6e818412ed9334a0a0cdcb4b8854fd531
vitocatz <40641469+vitorcato@users.noreply.github.com>
2023-06-18 23:17:36 -0300

    Update pup.js

--- pup.js
+++ pup.js
@@ -1,3 +1,5 @@
+openai.api_key = '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';
+
 async function resumir_cidade(texto, comprimento_resumo = 50) {
     const prompt = `Redija um texto sobre a cidade: ${cidade} contanto sua historia, população, riqueza e fatos importantes. No máximo de 4 parágrafos, com coerência, ortografia perfeita e ótimo para leitura.\nComprimento: ${comprimento_resumo}\n`;
   const completions = await openai.completions.create({
